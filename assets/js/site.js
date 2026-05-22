33f8df14943181a8a0afd03ff2a0ca50c6f601de
Donvesh <deveshd628@gmail.com>
2026-05-22 18:05:15 +0530

    Update webinar date to May 26

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,5 +1,5 @@
 (function () {
-  const target = new Date("2026-05-21T23:00:00Z").getTime();
+  const target = new Date("2026-05-26T23:00:00Z").getTime();
   const trackingKeys = [
     "utm_source",
     "utm_medium",
